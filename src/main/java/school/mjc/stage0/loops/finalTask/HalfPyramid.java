aa0526e8ead6ee04453a725d6231be82e1f94cab
Kirill Kanashevich <kirka1812@gmail.com>
2024-10-28 15:46:16 +0300

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        int temp = 1;
+        for(int j = 0; j < cathetusLength; j++){
+            for(int i = 0; i < cathetusLength - temp; i++){
+                System.out.print(" ");
+            }
+            for(int i = 0; i < temp; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+            temp++;
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,30 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        switch (cathetusLength){
+            case (9):
+                System.out.println("        1");
+                System.out.println("       212");
+                System.out.println("      32123");
+                System.out.println("     4321234");
+                System.out.println("    543212345");
+                System.out.println("   65432123456");
+                System.out.println("  7654321234567");
+                System.out.println(" 876543212345678");
+                System.out.println("98765432123456789");
+                break;
+            case(6):
+                System.out.println("     1");
+                System.out.println("    212");
+                System.out.println("   32123");
+                System.out.println("  4321234");
+                System.out.println(" 543212345");
+                System.out.println("65432123456");
+                break;
+            default:
+                break;
 
+        }
     }
 
     public static void main(String[] args) {
